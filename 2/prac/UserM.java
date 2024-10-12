04f9c41cc957a5c5d378e1352aa38942d28a9725
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-12 13:58:26 +0500

    Add files via upload

--- 2/prac/UserM.java
+++ 2/prac/UserM.java
@@ -3,16 +3,16 @@ public class UserM {
         UserManager userManager = new UserManager();
 
         // Добавление пользователей
-        User user1 = new User("Иван Иванов", "ivan@gmail.com", "Admin");
-        User user2 = new User("Анна Анновна", "anna@gmail.com", "User");
+        User user1 = new User("Иван Иванов", "ivan@example.com", "Admin");
+        User user2 = new User("Анна Смирнова", "anna@example.com", "User");
         userManager.addUser(user1);
         userManager.addUser(user2);
 
         // Обновление пользователя
-        userManager.updateUser("anna@gmail.com", "Анна Анновна", "Admin");
+        userManager.updateUser("anna@example.com", "Анна Смирнова", "Admin");
 
         // Удаление пользователя
-        userManager.removeUser("ivan@gmail.com");
+        userManager.removeUser("ivan@example.com");
 
         // Отображение всех пользователей
         userManager.displayUsers();
