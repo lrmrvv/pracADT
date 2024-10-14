c0f3ba76683b4c4d15f202ced3e55cbbde65b5ca
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-14 20:51:26 +0500

    Create 2.java

---
+++ 5/prac/2.java
@@ -0,0 +1,20 @@
+public class Report implements Document {
+    @Override
+    public void Open() {
+
+    }
+}
+
+public class Resume implements Document {
+    @Override
+    public void Open() {
+
+    }
+}
+
+public class Letter implements Document {
+    @Override
+    public void Open() {
+
+    }
+}
