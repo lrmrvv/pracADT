3735170a6e438de72972abe16d88d46e2d3d531c
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-20 17:24:58 +0500

    Create Beverage.java

---
+++ 8/DZ/Template Method/Beverage.java
@@ -0,0 +1,25 @@
+public abstract class Beverage {
+    public final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        if (customerWantsCondiments()) {
+            addCondiments();
+        }
+    }
+
+    protected abstract void brew();
+    protected abstract void addCondiments();
+
+    private void boilWater() {
+        System.out.println("Boiling water...");
+    }
+
+    private void pourInCup() {
+        System.out.println("Pouring into cup...");
+    }
+
+    protected boolean customerWantsCondiments() {
+        return true;
+    }
+}
