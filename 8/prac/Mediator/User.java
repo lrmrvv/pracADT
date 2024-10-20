b80961f76269902377ad307ad9c6395dba383ea2
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-20 19:19:01 +0500

    Create User.java

---
+++ 8/prac/Mediator/User.java
@@ -0,0 +1,25 @@
+public class User implements IUser {
+    private String name;
+    private IMediator mediator;
+
+    public User(String name, IMediator mediator) {
+        this.name = name;
+        this.mediator = mediator;
+    }
+
+    @Override
+    public void send(String message, String channelName) {
+        System.out.println(this.name + " sends message: " + message + " to channel: " + channelName);
+        mediator.sendMessage(message, this, channelName);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println(this.name + " received: " + message);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
