130feaa420a3fbbaeb7d59e52299b8205a0df061
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-20 19:21:39 +0500

    Update IMediator.java

--- 8/prac/Mediator/IMediator.java
+++ 8/prac/Mediator/IMediator.java
@@ -2,4 +2,5 @@ public interface IMediator {
     void sendMessage(String message, IUser user, String channelName);
     void addUser(IUser user, String channelName);
     void removeUser(IUser user, String channelName);
+    void sendPrivateMessage(String message, IUser fromUser, IUser toUser);
 }
