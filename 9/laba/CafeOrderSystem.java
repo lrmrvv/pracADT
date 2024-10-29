4128a720b68e3216394cd7837b0dba7663888659
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-29 08:38:54 +0500

    Create CafeOrderSystem.java

---
+++ 9/laba/CafeOrderSystem.java
@@ -0,0 +1,21 @@
+public class CafeOrderSystem {
+    public static void main(String[] args) {
+        IBeverage beverage = new Coffee();
+        System.out.println(beverage.getDescription() + " : " + beverage.getCost());
+
+        beverage = new MilkDecorator(beverage);
+        System.out.println(beverage.getDescription() + " : " + beverage.getCost());
+
+        beverage = new SugarDecorator(beverage);
+        System.out.println(beverage.getDescription() + " : " + beverage.getCost());
+
+        beverage = new ChocolateDecorator(beverage);
+        System.out.println(beverage.getDescription() + " : " + beverage.getCost());
+
+        beverage = new VanillaDecorator(beverage);
+        System.out.println(beverage.getDescription() + " : " + beverage.getCost());
+
+        beverage = new CinnamonDecorator(beverage);
+        System.out.println(beverage.getDescription() + " : " + beverage.getCost());
+    }
+}
