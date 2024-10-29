008237bf862d6edb48d4154e46505a008c7de77c
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-29 08:35:37 +0500

    Create MilkDecorator.java

---
+++ 9/laba/MilkDecorator.java
@@ -0,0 +1,16 @@
+class MilkDecorator extends BeverageDecorator {
+    public MilkDecorator(IBeverage beverage) {
+        super(beverage);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 10.0; 
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Milk";
+    }
+}
+
