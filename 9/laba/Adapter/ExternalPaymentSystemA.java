a48a6dd6fba37efbe44ace2a4309f4ac3f6e2968
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-10-29 08:58:24 +0500

    Update ExternalPaymentSystemA.java

--- 9/laba/Adapter/ExternalPaymentSystemA.java
+++ 9/laba/Adapter/ExternalPaymentSystemA.java
@@ -1,9 +1,9 @@
 class ExternalPaymentSystemA {
     public void makePayment(double amount) {
-        System.out.println("Making payment of " + amount + " via External Payment System A.");
+        System.out.println("Осуществление платежа " + amount + " через Внешнюю платежную систему A.");
     }
 
     public void makeRefund(double amount) {
-        System.out.println("Making refund of " + amount + " via External Payment System A.");
+        System.out.println("Осуществление возвратаMaking refund of " + amount + " через внешнюю платежную систему A.");
     }
 }
