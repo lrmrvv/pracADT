68d41c311a455af7dba88aa210687ba788f36d82
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-05 08:18:41 +0500

    Create LightingSystem.java

---
+++ 10/laba/fasad/LightingSystem.java
@@ -0,0 +1,13 @@
+public class LightingSystem {
+    public void turnOn() {
+        System.out.println("Свет включен.");
+    }
+
+    public void setBrightness(int level) {
+        System.out.println("Яркость подсветки установлена на " + level + ".");
+    }
+
+    public void turnOff() {
+        System.out.println("Свет выключен.");
+    }
+}
