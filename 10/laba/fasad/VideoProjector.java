717b2b55bfbb748c85ba784910e20f90193f010b
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-05 08:20:44 +0500

    Create VideoProjector.java

---
+++ 10/laba/fasad/VideoProjector.java
@@ -0,0 +1,13 @@
+public class VideoProjector {
+    public void turnOn() {
+        System.out.println("Видеопроектор включен.");
+    }
+
+    public void setResolution(String resolution) {
+        System.out.println("Разрешение видео установлено на " + resolution + ".");
+    }
+
+    public void turnOff() {
+        System.out.println("Видеопроектор выключен.");
+    }
+}
