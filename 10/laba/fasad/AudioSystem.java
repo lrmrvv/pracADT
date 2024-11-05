fb71a7cb0481e7e6b2ebcfbd864f14cc48620a95
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-05 08:19:36 +0500

    Update AudioSystem.java

--- 10/laba/fasad/AudioSystem.java
+++ 10/laba/fasad/AudioSystem.java
@@ -1,17 +1,13 @@
-public class AudioSystem
-{
-    public void TurnOn()
-    {
-        Console.WriteLine("Audio system is turned on.");
+public class AudioSystem {
+    public void turnOn() {
+        System.out.println("Аудиосистема включена.");
     }
 
-    public void SetVolume(int level)
-    {
-        Console.WriteLine($"Audio volume is set to {level}.");
+    public void setVolume(int level) {
+        System.out.println("Громкость звука установлена на " + level + ".");
     }
 
-    public void TurnOff()
-    {
-        Console.WriteLine("Audio system is turned off.");
+    public void turnOff() {
+        System.out.println("Аудиосистема выключена.");
     }
 }
