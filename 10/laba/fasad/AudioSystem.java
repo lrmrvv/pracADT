b09609c7e61b3e568eb7755c93727a1eedda50d7
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-05 08:15:45 +0500

    Create AudioSystem.java

---
+++ 10/laba/fasad/AudioSystem.java
@@ -0,0 +1,17 @@
+public class AudioSystem
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Audio system is turned on.");
+    }
+
+    public void SetVolume(int level)
+    {
+        Console.WriteLine($"Audio volume is set to {level}.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Audio system is turned off.");
+    }
+}
