95f92f96bc022e58cbca22a5710a04a33b8c70fd
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 13:56:09 +0500

    Create EventManagementSystem.java

---
+++ 10/prac/Facade/EventManagementSystem.java
@@ -0,0 +1,8 @@
+class EventManagementSystem {
+    public void bookConferenceHall() {
+        System.out.println("Конференц-зал забронирован.");
+    }
+    public void bookEquipment() {
+        System.out.println("Оборудование забронировано.");
+    }
+}
