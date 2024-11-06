9a65f344a12811a8aa976956ab8695585cc5c629
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 14:13:25 +0500

    Create GameConsole.java

---
+++ 10/DZ/GameConsole.java
@@ -0,0 +1,9 @@
+class GameConsole {
+    public void turnOn() {
+        System.out.println("Игровая консоль включена.");
+    }
+
+    public void startGame(String game) {
+        System.out.println("Запуск игры: " + game);
+    }
+}
