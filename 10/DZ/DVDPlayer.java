4f8b24f2577187eb0a41ac92c7a4756aa3db3170
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 14:13:06 +0500

    Create DVDPlayer.java

---
+++ 10/DZ/DVDPlayer.java
@@ -0,0 +1,13 @@
+class DVDPlayer {
+    public void play() {
+        System.out.println("Воспроизведение DVD.");
+    }
+
+    public void pause() {
+        System.out.println("DVD на паузе.");
+    }
+
+    public void stop() {
+        System.out.println("Воспроизведение DVD остановлено.");
+    }
+}
