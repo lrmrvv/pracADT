e9bd126c1f768c4800be7dfba2c1c1adb4cc4044
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 14:12:23 +0500

    Create TV.java

---
+++ 10/DZ/TV.java
@@ -0,0 +1,13 @@
+class TV {
+    public void turnOn() {
+        System.out.println("Телевизор включен.");
+    }
+
+    public void turnOff() {
+        System.out.println("Телевизор выключен.");
+    }
+
+    public void setChannel(int channel) {
+        System.out.println("Телевизор переключен на канал: " + channel);
+    }
+}
