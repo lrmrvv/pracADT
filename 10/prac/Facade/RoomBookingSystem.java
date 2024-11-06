b7ec7c6ae2f6f254eb633c1cc108316447c35a31
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 13:55:18 +0500

    Create RoomBookingSystem.java

---
+++ 10/prac/Facade/RoomBookingSystem.java
@@ -0,0 +1,11 @@
+class RoomBookingSystem {
+    public void bookRoom() {
+        System.out.println("Номер забронирован.");
+    }
+    public void checkAvailability() {
+        System.out.println("Проверка доступности номеров.");
+    }
+    public void cancelBooking() {
+        System.out.println("Бронирование номера отменено.");
+    }
+}
