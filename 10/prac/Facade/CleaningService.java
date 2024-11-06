e40ce204b96661e5d3ade7554f7effaddae4f92d
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 13:56:30 +0500

    Create CleaningService.java

---
+++ 10/prac/Facade/CleaningService.java
@@ -0,0 +1,8 @@
+class CleaningService {
+    public void scheduleCleaning() {
+        System.out.println("Уборка запланирована.");
+    }
+    public void performCleaning() {
+        System.out.println("Уборка выполнена.");
+    }
+}
