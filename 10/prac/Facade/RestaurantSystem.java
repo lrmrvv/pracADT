5b00243eb7945d9e82196dce79d87fccab048e35
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 13:55:37 +0500

    Create RestaurantSystem.java

---
+++ 10/prac/Facade/RestaurantSystem.java
@@ -0,0 +1,11 @@
+class RestaurantSystem {
+    public void bookTable() {
+        System.out.println("Столик в ресторане забронирован.");
+    }
+    public void orderFood() {
+        System.out.println("Еда заказана.");
+    }
+    public void callTaxi() {
+        System.out.println("Такси вызвано.");
+    }
+}
