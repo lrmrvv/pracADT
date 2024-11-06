9f27732876bc29a188f47587524cc70fe27a7e9a
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-11-06 14:12:42 +0500

    Create AudioSystem.java

---
+++ 10/DZ/AudioSystem.java
@@ -0,0 +1,13 @@
+class AudioSystem {
+    public void turnOn() {
+        System.out.println("Аудиосистема включена.");
+    }
+
+    public void turnOff() {
+        System.out.println("Аудиосистема выключена.");
+    }
+
+    public void setVolume(int level) {
+        System.out.println("Громкость установлена на уровень: " + level);
+    }
+}
